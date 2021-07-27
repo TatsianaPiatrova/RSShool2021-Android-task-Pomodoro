8fe8448ebf87ac074d8b7c11e26f5622f432d060
TatsianaPiatrova <67@#zxvc>
2021-07-27 14:33:16 +0300

    feat: 3 steps

---
+++ app/src/main/java/com/example/stopwatch/Utils.kt
@@ -0,0 +1,22 @@
+package com.example.stopwatch
+
+const val START_TIME = "00:00:00"
+
+fun Long.displayTime(): String {
+    if (this <= 0L) {
+        return START_TIME
+    }
+    val h = this / 1000 / 3600
+    val m = this / 1000 % 3600 / 60
+    val s = this / 1000 % 60
+
+    return "${displaySlot(h)}:${displaySlot(m)}:${displaySlot(s)}"
+}
+
+private fun displaySlot(count: Long): String {
+    return if (count / 10L > 0) {
+        "$count"
+    } else {
+        "0$count"
+    }
+}
